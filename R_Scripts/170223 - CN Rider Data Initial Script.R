38113e33d6d2a3b6e98a331a30637734aa36fe8c
SimonLyons <simon_lyons@hotmailcom>
2017-02-28 06:24:51 +1000

    I had a more successful time sort out the rider list webscrape today and I think I've got it sorted out. My test runs have been limited to the first few teams in any given year. I had two problems: 1. For some reason I was using the xPath for Nationality when I wanted the UCI ID data; and 2. I needed to find a way to extract just the UCI number. gsub came to the rescue!
    I can now generate an overall list of riders for a year. I think it will be useful to put each of these rider lists for each year into the database. I'll still want a master rider list that is a table with only one entry for each rider.

--- R_Scripts/170223 - CN Rider Data Initial Script.R
+++ R_Scripts/170223 - CN Rider Data Initial Script.R
@@ -1,12 +1,19 @@
 # Initial function for rider information
+# The function builds a dataframe of all the riders in each year along with
+# a range of related information: team, DOB, nationality, etc
 
+# Specify years for extraction of rider data
+# ONLY NEEDED FOR TESTING
+# start_year <- 2010
+# end_year <- 2010
 
+getRiderList <- function(start_year, end_year){
 
+# Require the XML and data.table packages
 library(XML)
+require(data.table)   # Allows me to use the rbindlist function towards the end
 
-start_year <- 2000
-end_year <- 2000
-
+# Run a FOR loop through all of the specified years. 
 for (p in start_year:end_year){
   my_url <- paste("http://www.cyclingnews.com/teams/", p, sep = "")
   my_url_parse <- htmlParse(my_url)
@@ -15,24 +22,24 @@ for (p in start_year:end_year){
   
   # Divide the xml data into team elements, divided by /tr node with 'data-i' inluded
   teams_xml <- xpathApply(my_url_parse, "//div[@class='team_box'][1]/ul/li/h3/a")
-  no_teams <- 2  # length(teams_xml)
+  no_teams <- length(teams_xml)
   
   team_table <- as.data.frame(matrix(data = NA, nrow = no_teams, ncol = 2 ))
   colnames(team_table)[1] <- "team.name"
   colnames(team_table)[2] <- "team.link"
   
   # Use Windows Progress Bar for each year
-  total <- 2  # no_teams
+  total <- no_teams
   # create progress bar
   pb <- winProgressBar(title = "Team Data Download Progress", label = "0% done",  min = 0,
                        max = total, width = 300)
   
   for (j in 1:no_teams){   # For loop that fills in table with all of the teams
     team_table[j,1] <- xmlValue(teams_xml[[j]])   # Team name
-    team_table[j,2] <- paste(xmlAttrs(teams_xml[[j]]), sep = "")   # URL link
+    team_table[j,2] <- paste(xmlAttrs(teams_xml[[j]]), sep = "")   # Team URL link
   }   # End for loop to run through number of teams
  
-  assign(paste("T.", p, sep = ""), team_table)   # Give unique year name to each table of teams
+  # assign(paste("T.", p, sep = ""), team_table)   # Give unique year name to each table of teams
   
   no_teams <- nrow(team_table)   # re-assign number of teams variable to table length (just to be safe)
   # Insert loop to run through all of the teams in the table of teams
@@ -63,28 +70,29 @@ for (p in start_year:end_year){
       rider_url_parse <- htmlParse(rider_url)
       # i_rider_xml <- xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']")
       
-      # i_team_table[k,4] <- xmlValue(i_rider_xml[[1]][[2]][[2]])   # Get rider DOB
-      i_team_table[k,4] <- xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']/div[@class='rider-data rider-dob']/span[@itemprop='birthDate']", xmlValue)
-      # i_team_table[k,5] <- xmlValue(i_rider_xml[[1]][[3]][[2]])   # Get rider nationality
-      i_team_table[k,5] <- xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']/div[@class='rider-data rider-nationality']/span[@itemprop='nationality']", xmlValue)
-      # i_team_table[k,6] <- xmlValue(i_rider_xml[[1]][[4]][[2]])   # Get rider UCI ID/number
-      if(length(xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']/div[@class='rider-data rider-uci']/span[@itemprop='nationality']", xmlValue))>0){
-        i_team_table[k,6] <- xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']/div[@class='rider-data rider-uci']/span[@itemprop='nationality']", xmlValue)
-        } else {
-          i_team_table[k,6] <- xmlValue(i_rider_xml[[1]][[4]][[2]])
-      }
+      # Get rider DOB
+      DOB <- xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']/div[@class='rider-data rider-dob']/span[@itemprop='birthDate']", xmlValue)
+      if(!is.null(DOB)){i_team_table[k,4] <- DOB} else {i_team_table[k,4] <- NA}
+      # Get rider nationality
+      nationality <- xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']/div[@class='rider-data rider-nationality']/span[@itemprop='nationality']", xmlValue)
+      if(!is.null(nationality)){i_team_table[k,5] <- nationality} else {i_team_table[k,5] <- NA}
+      # Get rider UCI ID/number
+      uci_pre <- xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']/div[@class='rider-data rider-uci']/span[@class='title']", xmlValue)
+      full_uci <- xpathApply(rider_url_parse, "//div[@class='rider-info-boxout']/div[@class='rider-data rider-uci']", xmlValue)
+      if(!is.null(uci_pre)){i_team_table[k,6] <- gsub(uci_pre, "", full_uci)} else {i_team_table[k,6] <- NA}
       
     }   # End LOOP through number of riders in each team
     
-    assign(paste("T.", p, ".", e, sep = ""), i_team_table)
-    
-    # team_table_list <- c(team_table_list, paste("T.", p, ".", e, sep = ""))
-    
+    # Add latest i_team_table to a list of tables for the year
+    team_table_list[[e]] <- assign(paste("T.", p, ".", e, sep = ""), i_team_table)
+
   }   # End LOOP through the number of teams in the table of teams (in a given year)
   close(pb)   # Windows Progress Bar script 
-  
-  # rider_list <- rbind(team_table_list)
+
+  # Combine the list of tables for the year into a single list of riders
+  assign(paste("riderlist", p, sep = ""), rbindlist(team_table_list))
   
 }   # End FOR loop to run through years
+}   # End getRiderList Function
 
 
