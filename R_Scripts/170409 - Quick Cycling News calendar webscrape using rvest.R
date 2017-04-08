02404996735d073556c7f099d691713d01b0665c
SimonLyons <simon_lyons@hotmail.com>
2017-04-09 08:49:22 +1000

    This is a quick bit of code I created after reading an 'rvest' tutorial, which was recommmended by colleagues at the UnConf as the best webscraping package. It was REALLY easy to pull down the Cycling News calendar tables, but I suspect I made things more involved in my code thanks to the need to extract non-continuous xml attribrutes - in this case the race_weblink data.

---
+++ R_Scripts/170409 - Quick Cycling News calendar webscrape using rvest.R
@@ -0,0 +1,23 @@
+# Quick bit of code using rvest to extract the calendar
+# data from Cycling News.
+# I think I may have gone with a more laborious loop method
+# in order to capture the race_weblink data, that isn't on 
+# every line of the tables.
+
+require(rvest)
+require(lubridate)
+my_url <- "http://www.cyclingnews.com/races/calendar/2011/"
+my_html <- read_html(my_url)
+
+cn_nodes <- html_nodes(my_html, "table")
+cn_calendar <- c()
+for(i in 1:length(cn_nodes)){
+  # extract the table data for each month
+  cn_table <- html_table(cn_nodes)[[i]]
+  # Combine the latest month table into the overall year table
+  cn_calendar <- rbind(cn_calendar, cn_table)
+}n_calendar
+# Add the year so this can be filtered in the master calendar table
+cn_calendar$year <- 2011   
+View(cn_calendar)
+
