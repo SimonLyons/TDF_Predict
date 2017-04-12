33addc07595dc674f50f76165c8d96920829b227
SimonLyons <simon_lyons@hotmail.com>
2017-04-13 05:57:22 +1000

    Updated database code R script to include remote connection to my MySQL server. It will be interesting to see if this works from Maleny.

--- R_Scripts/170212 - Database code v1.R
+++ R_Scripts/170212 - Database code v1.R
@@ -1,18 +1,28 @@
-
 # Testing database access skills
-
+# install.packages("RMySQL")
 require(RMySQL)
 
 # Create connection to local MySQL server
+# Local machine
+conn_local <- dbConnect(MySQL(), user='test_DB_manager', password='db_manager_45', host="")
+# Remote machine
 conn_local <- dbConnect(MySQL(), user='test_DB_manager', password='db_manager_45', host='localhost')
 
 # Create ProCycling stats database
 dbSendQuery(conn_local, "CREATE DATABASE ProCycling;")
 
-
-
 # Create connection to Procycling database
+# Local machine
 conn_local <- dbConnect(MySQL(), user='test_DB_manager', password='db_manager_45',  dbname='ProCycling', host='localhost')
+# Remote machine
+conn_local <- dbConnect(MySQL(), user='test_DB_manager', password='db_manager_45',  dbname='ProCycling', host='192.168.1.1', port=3306)
+
+# Script for closing all active connections to MySQL databases.
+all_cons <- dbListConnections(MySQL())
+for(con in all_cons) 
+  dbDisconnect(con)
+
+
 
 setwd("C:/b_Data_Analysis/Projects/TDF_Predict/Data_Files")
 
@@ -34,43 +44,24 @@ dbRemoveTable(conn_local, paste("calendar_cn_", i, sep = ""))
 # End script to remove tables
 #################################################################
 
-dbSendQuery(conn_local, "ALTER TABLE calendar_cn_2010 ADD PRIMARY KEY(`event.ID`);" )
-
-ALTER TABLE goods ADD PRIMARY KEY(id)
-
-dbSendQuery(conn_local, "ALTER TABLE race_calendar_2006 CHANGE COLUMN race_id race_id INT PRIMARY KEY;")
-dbSendQuery(conn_local, 'ALTER TABLE race_calendar_2006 ALTER PRIMARY KEY(race_id);')
-alter table Persion add primary key (persionId,Pname,PMID)
+# Some MySQL commands copied in early in my DB learning process
+# ALTER TABLE goods ADD PRIMARY KEY(id)
+# dbSendQuery(conn_local, "ALTER TABLE calendar_cn_2010 ADD PRIMARY KEY(`event.ID`);" )
+# dbSendQuery(conn_local, "ALTER TABLE race_calendar_2006 CHANGE COLUMN race_id race_id INT PRIMARY KEY;")
+# dbSendQuery(conn_local, 'ALTER TABLE race_calendar_2006 ALTER PRIMARY KEY(race_id);')
+# alter table Persion add primary key (persionId,Pname,PMID)
 
 # List db tables
 dbListTables(conn_local, dbname='ProCycling')
 
-
-
-
-
+# List 'fields' from particular table
 dbListFields(conn_local, "race_calendar_2005")
-
-
-query <- dbSendQuery(conn_local, "SELECT * FROM race_calendar_2005;")
-new_df <- dbFetch(query)
-
-query <- dbSendQuery(conn_local, "DESCRIBE race_calendar_2005;")
-new_df <- dbFetch(query)
-View(new_df)
 
 
 # List all open db connections
 dbListConnections(MySQL())
 
 dbClearResult(dbListResults(conn_local)[[1]])
-
-query <- dbSendQuery(conn_local, "SELECT * FROM calendar_cn_2010 limit 5000;")
-new_df <- dbFetch(query, n=500)
-new_df[480:489,"Web.link"]
-
-summary(new_df)
-new_df[, "Web.link"]
 
 # Get info about db that's connected
 dbGetInfo(conn_local)
@@ -87,6 +78,21 @@ dbDisconnect(dbListConnections(MySQL())[[1]])
 
 # Create connection to Procycling database
 conn_local <- dbConnect(MySQL(), user='test_DB_manager', password='db_manager_45',  dbname='ProCycling', host='localhost')
+
+# Test out some queries
+query <- dbSendQuery(conn_local, "SELECT * FROM race_calendar_2005;")
+new_df <- dbFetch(query)
+
+query <- dbSendQuery(conn_local, "DESCRIBE race_calendar_2005;")
+new_df <- dbFetch(query)
+View(new_df)
+
+query <- dbSendQuery(conn_local, "SELECT * FROM calendar_cn_2010 limit 5000;")
+new_df <- dbFetch(query, n=500)
+new_df[480:489,"Web.link"]
+
+summary(new_df)
+new_df[, "Web.link"]
 
 tables_query <- dbSendQuery(conn_local, "SHOW tables;")
 tables <- dbFetch(tables_query, n=-1)
