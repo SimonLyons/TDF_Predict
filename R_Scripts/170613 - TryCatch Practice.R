90584f843fde248f40ff7591255392c214493d3e
Simon P Lyons <simon.p.lyons@boeing.com>
2017-06-15 15:34:47 +1000

    Work on tryCatch() practice script.

--- R_Scripts/170613 - TryCatch Practice.R
+++ R_Scripts/170613 - TryCatch Practice.R
@@ -1,46 +1,58 @@
 
 
+# Set the number of years and the starting year.
+a <- 5   # Number of years
+y <- 2016   # Starting year
 
 # Set up a dataframe with some test cycling news calendar weblinks
-my_url <- as.data.frame(matrix(data = NA, nrow = 3, ncol = 2))
+my_url <- as.data.frame(matrix(data = NA, nrow = a, ncol = 2))
 colnames(my_url) <- c("Year", "url")
-my_url[ , 1] <- 2016:2018
+my_url[ , 1] <- y:(y+(a-1))
 my_url[ , 2] <- paste0("http://www.cyclingnews.com/road/races/calendar/", my_url[ , 1], "/")
 
 # Set the working directory (for work laptop)
 setwd("C:/aa Simon Lyons/2.0 Work/4.0 Data Analysis/4.1 Sandpit/")
 
-
 # Prepare a dataframe to collect error messages and locations
-error_counter <- 1
-error_list <- as.data.frame(matrix(data = NA, nrow = 0, ncol = 2))
-colnames(error_list) <- c("Year", "Error")
-
+# error_counter <- 1
+error_list <- as.data.frame(matrix(data = NA, nrow = 0, ncol = 3))
+colnames(error_list) <- c("Year", "Error", "Error_Counter")
+write.csv(error_list, "error_list.csv", row.names = FALSE)
 
 # Run through a loop downloading the url data and reporting errors and warnings
-for(c in 1:length(my_url)){
+for(c in 1:nrow(my_url)){
 
     suppressWarnings(tryCatch({
     download.file(my_url[c, 2], "my_url.xml")
     
   }, warning = function(war) {
     print(paste("MY_WARNING:  ", my_url[c, 1],war))
-    error_list[error_counter, 1] <- c
+    print(war)
+    error_list <- read.csv(file = "error_list.csv")
+    error_counter <- nrow(error_list) + 1
+    error_list[error_counter, 1] <- my_url[c, 1]
     error_list[error_counter, 2] <- war
-    error_counter <- error_counter + 1
+    error_list[error_counter, 3] <- error_counter
+    write.csv(error_list, "error_list.csv", row.names = FALSE)
+    # print(error_counter)
+    # print(error_list)
     return(war)
-  }, error = function(err) {
-    print(paste("MY_ERROR:  ",err))
-    error_list[error_counter, 1] <- c
-    error_list[error_counter, 2] <- err
-    error_counter <- error_counter + 1
-    return(err)
+  # }, error = function(err) {
+  #   print(paste("MY_ERROR:  ",err))
+  #   error_list <- read.csv(file = "error_list.csv")
+  #   error_counter <- nrow(error_list) + 1
+  #   error_list[error_counter, 1] <- c
+  #   error_list[error_counter, 2] <- err
+  #   write.csv(error_list, "error_list.csv", row.names = FALSE)
+    # return(err)
   }, finally = {
     
-    print(paste0("Through round: ", c))
-    ?warning
+    print(paste0("Through round: ", c, ", for year ", my_url[c, 1]))
   }   # End FINALLY
     
   ))   # End TryCatch
   
 }   # End of FOR loop running through my_url list
+
+
+error_list <- read.csv(file = "error_list.csv")
