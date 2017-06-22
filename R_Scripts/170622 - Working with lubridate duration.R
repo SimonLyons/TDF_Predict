130014ca5c47aa638453e6321adca2b65c440aa6
Simon P Lyons <simon.p.lyons@boeing.com>
2017-06-22 12:06:32 +1000

    Updated work on conversion of lubridate duration character values.

--- R_Scripts/170622 - Working with lubridate duration.R
+++ R_Scripts/170622 - Working with lubridate duration.R
@@ -1,33 +1,44 @@
 
 require(lubridate)
+require(dplyr)
 
 # Obtain 'duration' values stored in database
 master_time <- dbGetQuery(conn_local, "SELECT * FROM master_results_time LIMIT 10 OFFSET 300;")
 View(master_time)
 dur05 <-  master_time$Duration[5]
 dur10 <-  master_time$Duration[10]
-print(dur05)
 dur05 <- "1089s (~18.15 minutes)"
-print(dur10)
 dur10 <- "1148s (~19.13 minutes)"
 
+# Sadly, lubridate doesn't recognise its own formatted variables once stored as class character in the database
+# This function therefore returns an 'NA'
+convert_time_string <- as.duration(dur10)
 
 # Efforts to convert database duration string to duration
+# Use the following regex function to identify whether a character exists in the string and its location 
+# even if the returned value is TRUE/FALSE
+regexpr("s", dur10)
 
-gregexpr(pattern = "s", dur10, value = TRUE)
-?gregexpr
 
-minutes(as_date("6:03:02"))
-as_date()
+# Ths IF statement checks for the existence of the seconds 's' and if TRUE, does the conversion to
+# lubridate 'seconds'. It can then be manipulated as a time variable.
+if(regexpr("s", dur10)){
+  dur10 <- seconds(substr(dur10, 0, regexpr("s", dur10)-1))
+}
 class(dur10)
-lubridate::as_date(dur10, %s %M)
-dur10 - dur05
-lubridate::as.duration()
-?as.Date
+
+# Convert variable 'dur05' as well.
+if(regexpr("s", dur05)){
+  dur05 <- seconds(substr(dur05, 0, regexpr("s", dur05)-1))
+}
 
 
-substr(dur10, 0, grep(dur10, "s"))
+# Some practice manipulation.
 
+hour_time <- lubridate::as.duration("5 hours")
 
+seconds(convert_time_string)
 
-
+new_time <- dur10 - dur05
+period(new_time)
+new_time <- new_time * 200
