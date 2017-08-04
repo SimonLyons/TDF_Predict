8ea4194192882b4369f3c8075b90da56d5dfe1ed
SimonLyons <simon_lyons@hotmail.com>
2017-08-04 16:16:37 +1000

    Added new R Script for basic random forest analysis on first TDF GC position data.

---
+++ R_Scripts/170804 - Basic TDF GC Random Forest Analysis.R
@@ -0,0 +1,8 @@
+
+install.packages("caret")
+library(caret)
+library(ggplot2)
+library()
+
+setwd("/home/a_friend/data_analysis/projects/TDF_Predict/working_data/")
+results_df <- read.csv("anal_df_2016.csv", header = TRUE)
