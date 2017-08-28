b6d37059676749dbe30f823df1e9873b05fc8bb7
SimonLyons <simon_lyons@hotmail.com>
2017-08-29 06:11:48 +1000

    Working on simple TDF GC prediction using a variety of linear regression models.

--- R_Scripts/170825 - Basic TDF GC Regression Analysis.R
+++ R_Scripts/170825 - Basic TDF GC Regression Analysis.R
@@ -43,11 +43,15 @@ write.csv(input_data_non_NaN, "input_data_non_NaN.csv", row.names = FALSE)
 # 2. Create training and testing datasets
 ##################################
 
+setwd("/home/a_friend/data_analysis/projects/TDF_Predict/working_data/")
+input_data_non_NaN <- read.csv("input_data_non_NaN.csv", header = TRUE)
+
 set.seed(1977)
 my_split <- createDataPartition(input_data_non_NaN$FP_2016, p = 0.7, list = FALSE)
 
 input_data_train <- input_data_non_NaN[my_split, ]
 input_data_test <- input_data_non_NaN[-my_split, ]
+View(input_data_train)
 
 # Create scatterplot to assist in visually identifying input
 # variables with a strong relationship with predicting final GC position.
@@ -61,17 +65,54 @@ splom(input_data_train[ , 2:6 ], data = input_data_train)
 
 # Use caret package to create predictive model based on speficied method.
 lrm <- train(data = input_data_train, FP_2016 ~ GC_Mean + best_GC, method = "glm")
-
 summary(lrm)
 lrm$finalModel
 
+# Here's a bunch of other models using alternative regression analysis packages
+ANFIS_lrm <- train(data = input_data_train, FP_2016 ~ GC_Mean + best_GC, method = "ANFIS")
+
+BstLm_lrm <- train(data = input_data_train, FP_2016 ~ GC_Mean + best_GC, method = "BstLm")
+
+GFS.THRIFT_lrm <- train(data = input_data_train, FP_2016 ~ GC_Mean + best_GC, method = "GFS.THRIFT")
+
 # Use predictive model to calculate a finishing position using the test data
 lrm_results <- predict(lrm, input_data_test, type = 'raw')
+ANFIS_results <- predict(ANFIS_lrm, input_data_test, type = 'raw')
+BstLm_results <- predict(BstLm_lrm, input_data_test, type = 'raw')
+GFS.THRIFT_results <- predict(GFS.THRIFT_lrm, input_data_test, type = 'raw')
 
 # Shift the results into the testing dataframe and create a delta column for comparison
 input_data_test$lrm_results <- round(lrm_results, 0)
 input_data_test$lrm_delta <- abs(input_data_test$lrm_results - input_data_test$FP_2016)
+
+input_data_test$ANFIS_results <- round(ANFIS_results, 0)
+input_data_test$ANFIS_delta <- abs(input_data_test$ANFIS_results - input_data_test$FP_2016)
+
+input_data_test$BstLm_results <- round(BstLm_results, 0)
+input_data_test$BstLm_delta <- abs(input_data_test$BstLm_results - input_data_test$FP_2016)
+
+input_data_test$GFS.THRIFT_results <- round(GFS.THRIFT_results, 0)
+input_data_test$GFS.THRIFT_delta <- abs(input_data_test$GFS.THRIFT_results - input_data_test$FP_2016)
+
 View(input_data_test)
 
-mean(input_data_test$lrm_delta)   # Calculate mean of the delta (how far the prediction was off)
 
+##################################
+# 4. Determine accuracy of results
+##################################
+
+mean(input_data_test$lrm_delta)   # Calculate mean of the delta (how far the prediction was off)
+mean(input_data_test$ANFIS_delta)
+mean(input_data_test$BstLm_delta)
+mean(input_data_test$GFS.THRIFT_delta)
+
+input_data_train_diff <- input_data_train %>% 
+  mutate(mean(diff(as.integer([1 ,5:7]))))
+
+
+mean(diff(as.integer(results_df[2,3:7])), na.rm = TRUE)
+
+class(results_df$X2015_FP)
+
+diff(as.matrix(results_df[5,3:7]))
+diff(c(45, 23, 19, 34, 72))
