f0332d4188f6b95c86fd0c1545013d03bb985ca0
Simon P Lyons <simon.p.lyons@boeing.com>
2017-09-05 10:00:04 +1000

    Further update to my work on determing model predictive accuracy. Understanding RMSE and RMSED better.

--- R_Scripts/170905 - Regression Prediction Accuracy Practice.R
+++ R_Scripts/170905 - Regression Prediction Accuracy Practice.R
@@ -4,22 +4,31 @@
 # Create dataset
 set.seed(1977)
 x <- floor(runif(100, 0, 101))
-my_error <- 1 * runif(100)
+my_error <- 100 * runif(100)
 average_error <- mean(my_error)
 y <- round((4 + (3 * x) + my_error),3)
 my_data <- as.data.frame(cbind(x,y))
 
 # Run regression analysis
-library(caret)
+require(caret)
 
+# Calculate/determine logistic regression model
 lm_model <- train(data = my_data, y ~ x, method = "lm")
 lm_model$finalModel
 lm_model$results
-plot(my_data$x, my_data$y, col = "blue")
-abline(lm_model$finalModel$coefficients[1],lm_model$finalModel$coefficients[2], col = "red")
+
+# Plot results
+plot(my_data$x, my_data$y, col = "blue")   # Plot data points
+abline(lm_model$finalModel$coefficients[1],lm_model$finalModel$coefficients[2], col = "red")   # Plot predictive model line
 
 my_data$predict_lm <- predict(lm_model, my_data)
-my_data$RMSE <- sqrt((my_data$predict_lm - my_data$y)^2)
+my_data$RMSE <- (my_data$y - my_data$predict_lm)^2
+mean(my_data$RMSE)
+RMSE_calc <- sqrt(mean(my_data$RMSE))
+lm_model$results$RMSE
+RMSE_mean <- sum(sqrt(my_data$RMSE))/nrow(my_data)
+NRMSE <- RMSE_calc/mean(my_data$y)
+
 mean(my_data$RMSE)
 my_data$RMSED <- my_data$RMSE/mean(my_data$y)
 View(my_data)
@@ -28,3 +37,50 @@ mean(my_data$RMSED)
 lm_model2 <- lm(y ~ x, data = my_data)
 lm_model2$residuals
 crossprod(lm_model2$residuals)
+
+
+###############################################################
+# Run a loop that increases error, calculated a model, predicts 
+# results and then determines the accuracy for comparison.
+
+# Set error values
+error_values <- c(1, 10, 100, 1000)
+RMSE <- rep(NA, 4)
+RMSED <- rep(NA, 4)
+# Create empty dataframe for populating with error results
+error_df <- as.data.frame(matrix(data = NA, nrow = 4, ncol = 6))
+my_names <- rep(c("RMSE", "RMSED"), 3)
+my_models <- c(rep("lm",2), rep("glm", 2), rep("rf",2))
+colnames(error_df) <- paste(my_names, my_models, sep = "_")
+error_df <- cbind(error_values, error_df)
+
+for (e in 1:length(error_values)){
+  # Create dataset
+  set.seed(1977)
+  x <- floor(runif(100, 0, 101))
+  my_error <- error_values[e] * runif(100)
+  y <- round((4 + (3 * x) + my_error),3)
+  my_data <- as.data.frame(cbind(x,y))
+  
+  # Load required packages
+  require(caret)
+  # Calculate/determine logistic regression model
+  lm_model <- train(data = my_data, y ~ x, method = "lm")
+  glm_model <- train(data = my_data, y ~ x, method = "glm")
+  rf_model <- train(data = my_data, y ~ x, method = "rf")
+  
+  # Predict results
+  # my_data$predict_lm <- predict(lm_model, my_data)
+  # my_data$delta_squared <- (my_data$y - my_data$predict_lm)^2
+  
+  # Store error RMSE in 
+  error_df[e, 2] <- lm_model$results$RMSE
+  error_df[e, 3] <- lm_model$results$RMSE / mean(my_data$y)
+  error_df[e, 4] <- glm_model$results$RMSE
+  error_df[e, 5] <- glm_model$results$RMSE / mean(my_data$y)
+  error_df[e, 6] <- rf_model$results$RMSE
+  error_df[e, 7] <- rf_model$results$RMSE / mean(my_data$y)
+}
+
+View(error_df)
+
