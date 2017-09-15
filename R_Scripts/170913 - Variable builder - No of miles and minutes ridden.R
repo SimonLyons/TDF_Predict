91aa041a674ae77545cd202f94c9708373058634
SimonLyons <simon_lyons@hotmail.com>
2017-09-16 06:27:12 +1000

    I've been working on visualisation associated with my new rider total annual riding time data.

--- R_Scripts/170913 - Variable builder - No of miles and minutes ridden.R
+++ R_Scripts/170913 - Variable builder - No of miles and minutes ridden.R
@@ -3,11 +3,15 @@
 # kilometres and hours they've ridden each year, prior to
 # the start of the Tour de France.
 
+######################################################################
+# SETUP
 
 # Load required libraries
 require(RMySQL)
 require(lubridate)
 require(dplyr)
+require(ggplot2)
+
 
 # Read prior file with details of riders finishing the 2016 TdF
 setwd("/home/a_friend/data_analysis/projects/TDF_Predict/working_data/")
@@ -28,7 +32,9 @@ conn_local <- dbConnect(MySQL(), user = as.character(psswd[psswd$type== "Manager
                         password = as.character(psswd[psswd$type == "Manager", "password"]),  
                         dbname='ProCycling', host='192.168.1.7', port=3306, timeout=3600)
 
-
+######################################################################
+# MySQL QUERIES
+# 
 #          ### WHERE filter based on rider###
 # Build a WHERE criteria list of all the riders who completed the 2016 TdF
 WHERE_rider <- paste0("Rider = '", results_df$Rider[1], "'")
@@ -79,7 +85,6 @@ TdF_dates <- dbGetQuery(conn_local, "SELECT race_details, MIN(start_date)
                                                   FROM race_calendar_master
                                                   WHERE SOUNDEX(race_details) = SOUNDEX('Tour De France')
                                                   GROUP BY YEAR(start_date);")
-
 View(TdF_dates)
 
 # Build list of TdF start dates, limited to 2010 and above
@@ -108,6 +113,9 @@ date_table <- dbGetQuery(conn_local, criteria)
 write.csv(date_table, "date_table.csv", row.names = FALSE)
 View(date_table)
 
+######################################################################
+# MANIPULATE DATA FROM MySQL QUERY
+# 
 # Filter down the table to just rows containg 'Results' or 'results'
 # We don't want the 'General Classification', 'Young Rider' or other classifications
 just_results <- date_table %>% filter(grepl(('Results|results'), result_class))
@@ -122,5 +130,27 @@ setwd("/home/a_friend/data_analysis/projects/TDF_Predict/working_data/")
 write.csv(rider_duration, "rider_duration.csv", row.names = FALSE)
 
 # Reformat the table so the years are spread out as columns
+# This format is useful for me eye-balling the data, but ggplot
+# requires the data in the long format created above: rider_duration
 rider_duration_spread <- tidyr::spread(rider_duration, Year, Riding_Time)
+View(rider_duration_spread)
 write.csv(rider_duration_spread, "rider_duration_spread.csv", row.names = FALSE)
+
+######################################################################
+# DATA VISUALISATION
+# 
+# This is a straight plot of all of the rider annual riding hours totals for all of the riders
+# The result is more crazy art than insightful data visualisation!
+ggplot(data = rider_duration, aes(x = Year, y = Riding_Time_hrs)) + geom_line(aes(colour = Rider), size = 1.2) + 
+  labs(title ="Total Riding Time Per Year (hrs)", x = "Year", y = "Hours") + theme(legend.position = "none")
+
+
+# Build a dataframe with the above total riding hrs per year in addition
+# to each riders TdF finishing position from the 
+# results_df/"anal_df_2016_C&T.csv" dataframe
+combined_rider_data <- merge(results_df, rider_duration_spread, by.x = "Rider", by.y = "Rider", all = TRUE)
+View(combined_rider_data)
+
+# Need to munge the combined_rider_data dataframe into a long format
+# I'll first pull out the data for 2016 finishind position and total hrs
+
--- R_Scripts/170915 - Simon Gerrans Comparo.R
+++ R_Scripts/170915 - Simon Gerrans Comparo.R
@@ -4,7 +4,7 @@
 # (in this case Simon Gerrans) and then displaying some graphics
 # showing the change in their performance over their career
 
-########################################
+######################################################################
 # Load required libraries
 require(RMySQL)
 require(lubridate)
@@ -13,11 +13,11 @@ require(dplyr)
 # Set working directory
 setwd("/home/a_friend/data_analysis/projects/TDF_Predict/working_data/")
 
-########################################
+######################################################################
 # Define input variables
 rider <- "Simon Gerrans"
 
-########################################
+######################################################################
 # Setup database connection
 dbpsswddir <- set_db_password()
 setwd(dbpsswddir)
@@ -32,7 +32,7 @@ conn_local <- dbConnect(MySQL(), user = as.character(psswd[psswd$type== "Manager
                         password = as.character(psswd[psswd$type == "Manager", "password"]),  
                         dbname='ProCycling', host='192.168.1.7', port=3306, timeout=3600)
 
-########################################
+######################################################################
 # Build a MySQL query that filters on the rider (Simon Gerrans) and
 # returns their race results, including the race name (race_details).
 # This will come from the race_calendar_master & master_results_time tables
