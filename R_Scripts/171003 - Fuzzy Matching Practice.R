934d8f17f1c6eba32a77aabfbed2d8d3f27d6166
Simon P Lyons <simon.p.lyons@boeing.com>
2017-10-05 16:26:30 +1000

    Fuzzy matching practice.

--- R_Scripts/171003 - Fuzzy Matching Practice.R
+++ R_Scripts/171003 - Fuzzy Matching Practice.R
@@ -99,3 +99,70 @@ class(cn_start_list_split$Rider)
 class(cn_stage_1_results_table_split$Rider)
 
 my_new_list <- ClosestMatch3(cn_start_list_split$Rider_1, cn_stage_1_results_table_split$Rider_2)
+
+
+
+
+
+########################################
+###### @work script to answer forum question
+###### Includes work on combining Levenshtein and Soundex functions.
+
+# Closest match function - designed to use Levenshtein Distances to match strings.
+# https://stackoverflow.com/questions/5721883/agrep-only-return-best-matches
+
+require(RecordLinkage)
+ClosestMatch2 = function(string, stringVector){
+  distance = levenshteinSim(string, stringVector);
+  stringVector[distance == max(distance)]
+}
+
+# This is my addition to simply the application of the above string matching
+# function to matching a first string (vector) against a second string (vector).
+
+# The function returns a matching character vector of matching names from the
+# second character vector list.
+# This matching character vector can be inserted into the dataframe from which
+# the first list originated and can be used to merge or match data between the
+# dataframes.
+ClosestMatch3 <- function(stringVector1, stringVector2){
+  as.character(lapply(stringVector1, ClosestMatch2,stringVector2))
+}
+
+
+# Multiple Fuzzy String Matching   
+# I'm experimenting with better solutions for the question posted on the forum:
+# https://insite.web.boeing.com/culture/viewQuestion.do?questionId=291911#answerthis
+
+myText <- "Misspelled sentence: evrythng in trvl doumnt mispeled & trvlrs leav mispelled coments"
+
+myText1<-unlist(strsplit(myText," ")) ## agrep requires this form
+
+search_string <- c("travel", "mispelled")
+
+
+
+# The following is the shortform answer I posted on the Boeing R Users forum.
+require(RecordLinkage)
+rep(myText1, length(search_string))[sapply(search_string, levenshteinSim, myText1) > 0.3]
+
+# The above, with conversion to lowercase included
+rep(myText1, length(search_string))[sapply(tolower(search_string), levenshteinSim, tolower(myText1)) > 0.3]
+
+# Using a combination of Soundex and Levenshtein functions (on first testing) seems to produce the best result.
+rep(myText1, length(search_string))[sapply(soundex(search_string), levenshteinSim, soundex(myText1)) > 0.9]
+soundex(search_string)
+
+
+cycling_search_term <- "Daniel Moreno"
+cycling_search_string <- c("Daniel Moreno Fernández", "Daniel Martin", "Daniel Monty", "Daniel Onerom")
+
+
+soundex(cycling_search_term)
+cycling_search_string[levenshteinSim(cycling_search_term, cycling_search_string) == max(levenshteinSim(cycling_search_term, cycling_search_string))]
+
+sapply(soundex(cycling_search_term), levenshteinSim, soundex(cycling_search_string))
+
+soundex("Daniel")
+soundex("Moreno")
+soundex("Fernández")
